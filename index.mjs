90f728ee8f3029664ff80521e0b9014ecb221e33
Allen Helton <allenheltondev@gmail.com>
2024-09-19 08:47:10 -0500

    update source to respond to health checks

--- index.mjs
+++ index.mjs
@@ -1,19 +1,31 @@
 import { Client, GatewayIntentBits } from 'discord.js';
-if(process.env.ENVIRONMENT !== 'production'){
-  import('dotenv').then(dotenv => dotenv.config());
+import express from 'express';
+
+async function run() {
+  if (process.env.ENVIRONMENT !== 'production') {
+    const dotenv = await import('dotenv');
+    dotenv.config();
+  }
+
+  const app = express();
+  app.listen(8000, () => {
+    console.log('Listening on port 8000. This is for the AppRunner health check.');
+  });
+
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildMessageReactions,
+    ],
+  });
+
+  client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+  });
+
+  client.login(process.env.DISCORD_TOKEN);
 }
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMessageReactions,
-  ],
-});
-
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
-
-client.login(process.env.DISCORD_TOKEN);
+run();
