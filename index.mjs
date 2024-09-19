0c198fde1785135a332c3b02aaab89543ab5f143
Allen Helton <allenheltondev@gmail.com>
2024-09-19 08:22:52 -0500

    add environment variables

--- index.mjs
+++ index.mjs
@@ -1,4 +1,8 @@
 import { Client, GatewayIntentBits } from 'discord.js';
+if(process.env.ENVIRONMENT !== 'production'){
+  import('dotenv').then(dotenv => dotenv.config());
+}
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
